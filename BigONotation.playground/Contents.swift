42918c60e7930075d1b0bff3b8ea1fbce8c11ea4
Ambrose Mbayi <91916741+mbayi-ios@users.noreply.github.com>
2023-07-18 13:27:13 +0300

    making the algorithm efficient by using hashtable

--- BigONotation.playground/Contents.swift
+++ BigONotation.playground/Contents.swift
@@ -83,7 +83,7 @@ func quadratic(_ n: Int) -> Int {
  
  */
 
-// Naive Brute Force
+//SOLUTION 1:  Naive Brute Force
 
 func commonItemsInArray(_ A: [Int], _ B: [Int]) -> Bool {
     //let before = Date()
@@ -109,4 +109,38 @@ commonItemsInArray([1,2,4,5], [1,3,5,6])
 
 /* notes on the above bruteforce method.
   - It is quadratic in nature O(n^2) because we have nested loops - Quadradic are slower. We can do better
+ */
+
+
+// SOLUTION 2: Using Hash Map
+
+func commonItemsInArrayHashMap(_ A: [Int], _ B: [Int]) -> Bool {
+    var hasA = [Int: Bool]()
+    
+    // stil looping __ but not nested - O(2n) vs O(n^2)
+    for a in A { // O(n)
+        hasA[a] = true
+    }
+    
+    // now lookup in the hash to see if elements of B exist
+    for b in B {
+        if hasA[b] == true {
+            return true
+        }
+    }
+    
+    return false
+}
+
+commonItemsInArrayHashMap([1, 3, 4], [10, 35, 5])
+
+/*Trading Off Time for Space
+ 
+    - Brute Force Time Complexity is O(n^2) and space complexity is O(1) - ie. we are not creating any more memory
+    - Hash Map Time Complexity is O(n) and space complexity is O(n) since we are creating new memory to hold the array
+ 
+ CONCLUSION
+  - Bruteforce is very space efficient however very slow.
+  - For Hash Map we are trading off space by creating extra data structure(memory)  but we get big improvement in terms of performance.
+  - Trading off Space for Time ;- important concept in interviews. MAKING Hash Table Go to
  */
